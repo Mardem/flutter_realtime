2225bd2a29d3bef8e5b92ccad07e2e54ec5a2280
Marden Cavalcante <main@marden.dev>
2025-07-14 19:14:49 -0300

    feat: Adicionando arquitetura pura

--- lib/modules/home/di/home_di.dart
+++ lib/modules/home/di/home_di.dart
@@ -1,8 +1,6 @@
 import 'package:get_it/get_it.dart';
 
 import '../../../core/core.dart';
-import '../../../core/remote/mqtt/di/mqtt_factory.dart';
-import '../../../core/remote/mqtt/domain/mqtt_repository.dart';
 import '../../../main.dart';
 
 class HomeDI extends BaseServiceLocator {
@@ -10,15 +8,7 @@ class HomeDI extends BaseServiceLocator {
   GetIt locator = inject;
 
   @override
-  Future<void> setServices() async {
-    inject.registerFactory<MqttRepository>(
-      () => MqttFactory.create(
-        server: 'test.mosquitto.org',
-        clientIdentifier:
-            'flutter_client_${DateTime.now().millisecondsSinceEpoch}',
-      ),
-    );
-  }
+  Future<void> setServices() async {}
 
   @override
   Future<void> setRepositories() async {}
--- lib/modules/home/presentation/home_presentation.dart
+++ lib/modules/home/presentation/home_presentation.dart
@@ -1,36 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_realtime/main.dart';
 
-import '../../../core/remote/mqtt/domain/mqtt_repository.dart';
-
-class HomePresentation extends StatefulWidget {
+class HomePresentation extends StatelessWidget {
   const HomePresentation({super.key});
-
-  @override
-  State<HomePresentation> createState() => _HomePresentationState();
-}
-
-class _HomePresentationState extends State<HomePresentation> {
-  final String _mqttTopic = 'flutter_chat/response';
-
-  final MqttRepository _mqttRepo = inject<MqttRepository>();
-
-  final TextEditingController _topicCtrl = TextEditingController(
-    text: 'flutter_chat/response',
-  );
-  final TextEditingController _messageCtrl = TextEditingController(
-    text: 'Olá, essa é minha primeira mensagem!',
-  );
-
-  @override
-  void initState() {
-    super.initState();
-    _initMqttClient();
-  }
-
-  Future<void> _initMqttClient() async {
-    await _mqttRepo.initialize().then((_) => _mqttRepo.subscribe(_mqttTopic));
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,56 +13,7 @@ class _HomePresentationState extends State<HomePresentation> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Flutter Realtime - MQTT'),
-              TextField(
-                controller: _topicCtrl,
-                decoration: InputDecoration(labelText: 'Tópico'),
-              ),
-              TextField(
-                controller: _messageCtrl,
-                decoration: InputDecoration(labelText: 'Mensagem'),
-              ),
-              SizedBox(height: 12),
-              Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      final topic = _topicCtrl.text;
-                      final msg = _messageCtrl.text;
-                      _mqttRepo.publish(topic, {'message': msg});
-                    },
-                    child: Text('Publicar'),
-                  ),
-                  SizedBox(width: 8),
-                  ElevatedButton(
-                    onPressed: () => _mqttRepo.clearMessages(),
-                    child: Text('Limpar mensagens'),
-                  ),
-                ],
-              ),
-              Expanded(
-                child: ValueListenableBuilder<List<String>>(
-                  valueListenable: _mqttRepo.messages,
-                  builder: (context, messages, _) {
-                    if (messages.isEmpty) {
-                      return Center(
-                        child: Text('Nenhuma mensagem recebida ainda.'),
-                      );
-                    }
-                    return ListView.builder(
-                      itemCount: messages.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(messages[index]),
-                          dense: true,
-                        );
-                      },
-                    );
-                  },
-                ),
-              ),
-            ],
+            children: [Text('Flutter Realtime - MQTT')],
           ),
         ),
       ),
