1e610c4a18db1a32e37a756f6ddacb6658783a1e
Marden Cavalcante <main@marden.dev>
2025-08-11 18:55:08 -0300

    feat: Adicionando client http completo no core

---
+++ lib/core/remote/http/data/client.dart
@@ -0,0 +1,102 @@
+import 'package:dio/dio.dart';
+
+import '../domain/http_client_interface.dart';
+import '../domain/http_response_interface.dart';
+
+class HttpClientImpl implements HttpClient {
+  final Dio _client;
+
+  final String? baseUrl;
+  final String? apiKey;
+
+  HttpClientImpl({
+    this.baseUrl,
+    this.apiKey,
+  }) : _client = Dio(
+         BaseOptions(
+           baseUrl: baseUrl ?? '',
+         ),
+       );
+
+  @override
+  Future<HttpResponse<dynamic>> get(
+    String path, {
+    String? baseUrl,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    final request = await _client.get(
+      path,
+      queryParameters: queryParameters,
+      options: options,
+    );
+
+    final response = HttpResponse();
+    response.data = request.data;
+
+    return response;
+  }
+
+  @override
+  Future<HttpResponse<dynamic>> post(
+    String path, {
+    String? baseUrl,
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    final request = await _client.post(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+
+    final response = HttpResponse();
+    response.data = request.data;
+
+    return response;
+  }
+
+  @override
+  Future<HttpResponse<dynamic>> put(
+    String path, {
+    String? baseUrl,
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    final request = await _client.put(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+
+    final response = HttpResponse();
+    response.data = request.data;
+
+    return response;
+  }
+
+  @override
+  Future<HttpResponse<dynamic>> delete(
+    String path, {
+    String? baseUrl,
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    final request = await _client.delete(
+      path,
+      data: data,
+      queryParameters: queryParameters,
+      options: options,
+    );
+
+    final response = HttpResponse();
+    response.data = request.data;
+
+    return response;
+  }
+}
--- lib/startup/di.dart
+++ lib/startup/di.dart
@@ -1,7 +1,16 @@
+import '../core/remote/http/data/client.dart';
+import '../core/remote/http/http.dart';
+import '../main.dart';
 import '../modules/home/di/home_di.dart';
 
 class InitAppModules {
   static Future<void> start() async {
+    inject.registerSingleton<HttpClient>(
+      HttpClientImpl(
+        baseUrl: 'https://restcountries.com/v3.1',
+      ),
+    );
+
     await HomeDI().initiate();
   }
 }
